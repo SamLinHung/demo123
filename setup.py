89ec2c3685af4e6f69b8a333eb1fce580068df52
SamLinHungYuan <slin@dojotechs.com>
2022-03-22 16:24:50 +0800

    0.0.21

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="dojotest",  
-    version="0.0.20",
+    version="0.0.21",
     author="HungYuanLin",
     author_email="slin@dojotechs.com",
     description="A automatic trading application of package",
