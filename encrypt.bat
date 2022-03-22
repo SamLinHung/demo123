c0d205c40ad4fa021a916da6a87d3e2bcc15e35c
SamLinHungYuan <slin@dojotechs.com>
2022-03-22 15:24:01 +0800

    0.0.15

--- encrypt.bat
+++ encrypt.bat
@@ -2,7 +2,7 @@ set python_version=%1%
 set dirName=dojo
 
 cd %dirName%
-python37 setup.py build_ext --inplace
+python setup.py build_ext --inplace
 dir
 xcopy .\build\lib.win-amd64-%python_version%\%dirName%\ .
 del .\ib.py
