08e44c682cc09634b9aa67192d66b879c4cba211
SamLinHungYuan <slin@dojotechs.com>
2022-05-03 19:02:22 +0800

    change setup

--- encryption/setup.py
+++ encryption/setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 setup(
+    name='Hello world app',
+    package_dir={'cython_test': ''},
     ext_modules=cythonize(["ib.py",
                            "base/auth.py"])
     
